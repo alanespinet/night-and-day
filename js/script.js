de2092edb140fe51d6002c4a1827d5b68657f6b5
alanespinet <aelluvetaa@gmail.com>
2016-06-22 21:57:13 -0400

    finished project

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$("#orb").on("click", function()
+{
+  if($("#orb").hasClass("sun"))
+  {
+    $("#orb").removeClass("sun").addClass("moon");
+    $("#sky").removeClass("day").addClass("night");
+  }
+  else
+  {
+    $("#orb").removeClass("moon").addClass("sun");
+    $("#sky").removeClass("night").addClass("day");
+  }
+});
